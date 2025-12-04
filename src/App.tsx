79ce755b59ad432b2b0dfcfa5c47a8e81c3a95e1
Emmanuella Batubo <emmanuellabatubo@icloud.com>
2025-12-04 14:55:07 +0100

    chore: setup Vite with SCSS and absolute imports

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Setup Complete</h1>
+    </div>
+  );
+}
+
+export default App;
